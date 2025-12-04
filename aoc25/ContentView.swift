7c24edb476314c8f6994e1089927f64481633ecd
Valentin Werner <valwerner2@gmail.com>
2025-12-04 12:06:12 +0100

    init

---
+++ aoc25/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aoc25
+//
+//  Created by Valentin Werner on 04.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aoc25/aoc25App.swift
@@ -0,0 +1,17 @@
+//
+//  aoc25App.swift
+//  aoc25
+//
+//  Created by Valentin Werner on 04.12.25.
+//
+
+import SwiftUI
+
+@main
+struct aoc25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
