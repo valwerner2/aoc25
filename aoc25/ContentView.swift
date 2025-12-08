05db1814277ec3a2142aba57cd95e359a3965aa4
Valentin Werner <valwerner2@gmail.com>
2025-12-08 16:29:08 +0100

    8-1

--- aoc25/ContentView.swift
+++ aoc25/ContentView.swift
@@ -8,53 +8,113 @@
 import SwiftUI
 internal import UniformTypeIdentifiers
 
+struct Vec3: Equatable, Hashable{
+    let x: Int
+    let y: Int
+    let z: Int
+    
+    func distanceTo(other: Vec3) -> Double{
+        sqrt(pow(Double(x-other.x), 2) + pow(Double(y-other.y), 2) + pow(Double(z-other.z), 2))
+    }
+}
+
 func processString(str: String) -> String{
-    var grid = str.split{$0.isNewline}.map { line in
-        line.map{String($0)}
+    let start = CFAbsoluteTimeGetCurrent()
+    
+    let grid = str.split{$0.isNewline}.map { line in
+        line.split{$0 == ","}
     }
-    grid.forEach{ line in
-        print(line)
+    
+    let vecs: [Vec3] = grid.compactMap { parts in
+        guard parts.count == 3,
+              let x = Int(parts[0]),
+              let y = Int(parts[1]),
+              let z = Int(parts[2]) else {
+            return nil
+        }
+        return Vec3(x: x, y: y, z: z)
     }
+    
+    var vecPairs: [(Vec3, Vec3)] = []
+    
+    for i in vecs.indices.dropLast(){
+        for j in (i + 1)..<vecs.count{
+            vecPairs.append((vecs[i], vecs[j]))
+        }
+    }
+    
+    print(vecPairs.count)
+    
+    vecPairs.sort { pairA, pairB in
+        let distA = pairA.0.distanceTo(other: pairA.1)
+        let distB = pairB.0.distanceTo(other: pairB.1)
+        return distA < distB
+    }
+    
     var result = 0
     
-    for x in grid[0].indices {
-        if(grid[0][x] == "S"){
-            grid[1][x] = "1"
-        }
-    }
-    print("--------------------")
-    grid.forEach{ line in
-        print(line)
-    }
-    for y in grid.indices.dropFirst(1).dropLast(){
-        for x in grid[0].indices {
-            if grid[y][x] != "." && grid[y][x] != "^" {
-                
-                if grid[y+1][x] == "." {grid[y+1][x] = grid[y][x]}
-                }
-            }
-        for x in grid[0].indices {
-            if grid[y][x] != "." && grid[y][x] != "^" {
-                let currentNum = Int(grid[y][x]) ?? 0
-                
-                if grid[y+1][x] == "^"{
-                    if grid[y+1][x + 1] == "." {grid[y+1][x + 1] = grid[y][x]}
-                    else{grid[y+1][x + 1] = String(currentNum + (Int(grid[y+1][x + 1]) ?? 0))}
-                    
-                    if grid[y+1][x - 1] == "." {grid[y+1][x - 1] = grid[y][x]}
-                    else{grid[y+1][x - 1] = String(currentNum + (Int(grid[y+1][x - 1]) ?? 0))}
-                }
-            }
-        }
-        print("--------------------")
-        grid.forEach{ line in
-            print(line)
-        }
-    }
-    for x in grid[0].indices {
-        result += Int(grid[grid.count - 1][x]) ?? 0
+    let amountToDo = 1000
+    for i in 0..<amountToDo{
+        print(vecPairs[i])
     }
     
+    
+    var circuits: [[Vec3]] = []
+    
+    for currentRun in 0..<amountToDo {
+        //check if one of the pairs already is part of circuit
+        
+        var containsFirst = false
+        var containsSecond = false
+        
+        var indexFirst: Int?
+        var indexSecond: Int?
+        
+        var i = 0
+        while i < circuits.count && (!containsFirst || !containsSecond){
+            
+            if circuits[i].contains(vecPairs[currentRun].0){
+                containsFirst = true
+                indexFirst = i
+            }
+            if circuits[i].contains(vecPairs[currentRun].1){
+                containsSecond = true
+                indexSecond = i
+            }
+            i += 1
+        }
+        
+        if let iFirst = indexFirst, let iSecond = indexSecond, iFirst != iSecond{
+            circuits[iFirst] = Array(Set(circuits[iFirst] + circuits[iSecond]))
+            circuits[iSecond] = []
+        }
+        else if containsFirst, !containsSecond, let iFirst = indexFirst{
+            circuits[iFirst].append(vecPairs[currentRun].1)
+        }
+        else if !containsFirst, containsSecond, let iSecond = indexSecond{
+            circuits[iSecond].append(vecPairs[currentRun].0)
+        }
+        else if indexFirst == nil && indexSecond == nil{
+            circuits.append([vecPairs[currentRun].0, vecPairs[currentRun].1])
+        }
+    }
+    circuits.sort {a, b in
+        return a.count > b.count
+    }
+    
+    circuits.forEach { curr in
+        print(curr.count)
+    }
+    
+    result = circuits[0].count
+    for i in 1..<3{
+        result *= circuits[i].count
+    }
+    
+    
+    let end = CFAbsoluteTimeGetCurrent()
+    let elapsedMs = (end - start) * 1000
+    print("Processing took \(elapsedMs) ms")
     return String(result)
 }
 
